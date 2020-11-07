fada0c90ee6b99bcc9158e945fcbdc3f0b2c092a
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2020-11-07 15:11:07 +0100

    Épisode 1 : Création du projet et du joueur

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private PlayerMotor motor;
+
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        // Calculer la vélocité (vitesse) du mouvement de notre joueur
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 moveHorizontal = transform.right * xMov;
+        Vector3 moveVertical = transform.forward * zMov;
+
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * speed;
+    }
+}
