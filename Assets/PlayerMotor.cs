bcef77edaddff768839b6e3cefd0c7dfbddf7755
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2020-11-11 16:34:13 +0100

    Épisode 02 : Mouvements du joueur

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -3,5 +3,52 @@
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMotor : MonoBehaviour
 {
+    [SerializeField]
+    private Camera cam;
 
+    private Vector3 velocity;
+    private Vector3 rotation;
+    private Vector3 cameraRotation;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void Rotate(Vector3 _rotation)
+    {
+        rotation = _rotation;
+    }
+
+    public void RotateCamera(Vector3 _cameraRotation)
+    {
+        cameraRotation = _cameraRotation;
+    }
+
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    private void PerformMovement()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    private void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+        cam.transform.Rotate(-cameraRotation);
+    }
 }
