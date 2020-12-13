f38a5cdd8ffdc24aee5889d1c3746c92c89ac67b
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2020-12-13 14:41:58 +0100

    Episode 07 : Système de dégâts

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private const string playerIdPrefix = "Player";
+
+    private static Dictionary<string, Player> players = new Dictionary<string, Player>();
+
+    public static void RegisterPlayer(string netID, Player player)
+    {
+        string playerId = playerIdPrefix + netID;
+        players.Add(playerId, player);
+        player.transform.name = playerId;
+    }
+
+    public static void UnregisterPlayer(string playerId)
+    {
+        players.Remove(playerId);
+    }
+
+    public static Player GetPlayer(string playerId)
+    {
+        return players[playerId];
+    }
+}
