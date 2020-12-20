5d8da61ceacb820021968d6dc2164f205ddf493d
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2020-12-20 15:32:50 +0100

    Episode 08 : Respawn

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,27 +1,111 @@
 ﻿using UnityEngine;
 using Mirror;
+using System.Collections;
 
 public class Player : NetworkBehaviour
 {
+    [SyncVar]
+    private bool _isDead = false;
+    public bool isDead
+    {
+        get { return _isDead; }
+        protected set { _isDead = value; }
+    }
+
     [SerializeField]
     private float maxHealth = 100f;
 
     [SyncVar]
     private float currentHealth;
 
-    private void Awake()
+    [SerializeField]
+    private Behaviour[] disableOnDeath;
+    private bool[] wasEnabledOnStart;
+
+    public void Setup()
     {
+        wasEnabledOnStart = new bool[disableOnDeath.Length];
+        for (int i = 0; i < disableOnDeath.Length; i++)
+        {
+            wasEnabledOnStart[i] = disableOnDeath[i].enabled;
+        }
+
         SetDefaults();
     }
 
     public void SetDefaults()
     {
+        isDead = false;
         currentHealth = maxHealth;
+
+        for (int i = 0; i < disableOnDeath.Length; i++)
+        {
+            disableOnDeath[i].enabled = wasEnabledOnStart[i];
+        }
+
+        Collider col = GetComponent<Collider>();
+        if(col != null)
+        {
+            col.enabled = true;
+        }
     }
 
-    public void TakeDamage(float amount)
+    private IEnumerator Respawn()
     {
+        yield return new WaitForSeconds(GameManager.instance.matchSettings.respawnTimer);
+        SetDefaults();
+        Transform spawnPoint = NetworkManager.singleton.GetStartPosition();
+        transform.position = spawnPoint.position;
+        transform.rotation = spawnPoint.rotation;
+    }
+
+    private void Update()
+    {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            RpcTakeDamage(999);
+        }
+    }
+
+    [ClientRpc]
+    public void RpcTakeDamage(float amount)
+    {
+        if(isDead)
+        {
+            return;
+        }
+
         currentHealth -= amount;
         Debug.Log(transform.name + " a maintenant : " + currentHealth + " points de vies.");
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        isDead = true;
+
+        for (int i = 0; i < disableOnDeath.Length; i++)
+        {
+            disableOnDeath[i].enabled = false;
+        }
+
+        Collider col = GetComponent<Collider>();
+        if (col != null)
+        {
+            col.enabled = false;
+        }
+
+        Debug.Log(transform.name + " a été éliminé.");
+
+        StartCoroutine(Respawn());
     }
 }
--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -48,7 +48,7 @@ public class PlayerShoot : NetworkBehaviour
         Debug.Log(playerId + " a été touché.");
 
         Player player = GameManager.GetPlayer(playerId);
-        player.TakeDamage(damage);
+        player.RpcTakeDamage(damage);
     }
 
 }
