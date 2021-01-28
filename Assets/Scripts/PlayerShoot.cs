a4367596450d409fc55f15ea46c047117202be37
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2021-01-28 13:45:53 +0100

    Episode 11 : Visuel de l'arme

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -3,7 +3,14 @@ using Mirror;
 
 public class PlayerShoot : NetworkBehaviour
 {
-    public PlayerWeapon weapon;
+    [SerializeField]
+    private PlayerWeapon weapon;
+
+    [SerializeField]
+    private GameObject weaponGFX;
+
+    [SerializeField]
+    private string weaponLayerName = "Weapon";
 
     [SerializeField]
     private Camera cam;
@@ -18,6 +25,8 @@ public class PlayerShoot : NetworkBehaviour
             Debug.LogError("Pas de caméra renseignée sur le système de tir.");
             this.enabled = false;
         }
+
+        weaponGFX.layer = LayerMask.NameToLayer(weaponLayerName);
     }
 
     private void Update()
