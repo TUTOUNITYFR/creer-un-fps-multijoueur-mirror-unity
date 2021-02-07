9f9fe61dd165816a4ef3259a08a0b547c442a5c0
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2021-02-07 14:05:03 +0100

    Episode 12 : Tir automatique
    + Modification sur le système de gestion des armes

---
+++ Assets/WeaponManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using Mirror;
+
+public class WeaponManager : NetworkBehaviour
+{
+
+    [SerializeField]
+    private PlayerWeapon primaryWeapon;
+
+    private PlayerWeapon currentWeapon;
+
+    [SerializeField]
+    private Transform weaponHolder;
+
+    [SerializeField]
+    private string weaponLayerName = "Weapon";
+
+    void Start()
+    {
+        EquipWeapon(primaryWeapon);
+    }
+
+    public PlayerWeapon GetCurrentWeapon()
+    {
+        return currentWeapon;
+    }
+
+    void EquipWeapon(PlayerWeapon _weapon)
+    {
+        currentWeapon = _weapon;
+
+        GameObject weaponIns = Instantiate(_weapon.graphics, weaponHolder.position, weaponHolder.rotation);
+        weaponIns.transform.SetParent(weaponHolder);
+
+        if(isLocalPlayer)
+        {
+            SetLayerRecursively(weaponIns, LayerMask.NameToLayer(weaponLayerName));
+        }
+    }
+
+    private void SetLayerRecursively(GameObject obj, int newLayer)
+    {
+        obj.layer = newLayer;
+
+        foreach (Transform child in obj.transform)
+        {
+            SetLayerRecursively(child.gameObject, newLayer);
+        }
+    }
+
+}
