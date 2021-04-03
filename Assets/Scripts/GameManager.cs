9e39f023a93e65f999f969653a1b5d40b5b166f7
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2021-04-03 12:09:32 +0200

    Épisode 19 : Ajout du killfeed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,6 +15,9 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private GameObject sceneCamera;
 
+    public delegate void OnPlayerKilledCallback(string player, string source);
+    public OnPlayerKilledCallback onPlayerKilledCallback;
+
     private void Awake()
     {
         if(instance == null)
