356c34f5742bf47850a5adb71903b30061242bc9
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2021-04-18 15:25:44 +0200

    Ajout d'autres armes

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PauseMenu : NetworkBehaviour
+{
+    public static bool isOn = false;
+
+    private NetworkManager networkManager;
+
+    private void Start()
+    {
+        networkManager = NetworkManager.singleton;
+    }
+
+    public void LeaveRoomButton()
+    {
+        if(isClientOnly)
+        {
+            networkManager.StopClient();
+        }
+        else
+        {
+            networkManager.StopHost();
+        }
+    }
+}
---
+++ Assets/Scripts/WeaponData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "WeaponData", menuName = "My Game/Weapon Data")]
+public class WeaponData : ScriptableObject
+{
+    public string name = "Submachine Gun";
+    public float damage = 10f;
+    public float range = 100f;
+
+    public float fireRate = 0f;
+
+    public GameObject graphics;
+}
