2ed5f155af55a67b1ef1672ce13c923c192a7a25
TUTO UNITY FR <32209410+TUTOUNITYFR@users.noreply.github.com>
2021-05-24 15:59:47 +0200

    Episode 24 : Affichage pseudo et barre de vie en jeu

---
+++ Assets/Scripts/CameraFacingBillboard.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CameraFacingBillboard : MonoBehaviour
+{
+    void Update()
+    {
+        Camera cam = Camera.main;
+
+        transform.LookAt(transform.position + cam.transform.rotation * Vector3.forward, cam.transform.rotation * Vector3.up);
+    }
+}
---
+++ Assets/Scripts/PlayerNameplate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerNameplate : MonoBehaviour
+{
+    [SerializeField]
+    private Text usernameText;
+
+    [SerializeField]
+    private RectTransform healthBarFill;
+
+    [SerializeField]
+    private Player player;
+
+    void Update()
+    {
+        usernameText.text = player.username;
+        healthBarFill.localScale = new Vector3(player.GetHealthPct(), 1f, 1f);
+    }
+}
